0978841fb57a5da19b852404ee4a0cd1bfe8edb1
Wilfred Lai <wilfred.lai@igsl-group.com>
2024-06-24 14:37:35 +0800

    Add import

--- backend/src/main/java/com/hkgov/csb/eproof/controller/TemplateController.java
+++ backend/src/main/java/com/hkgov/csb/eproof/controller/TemplateController.java
@@ -17,10 +17,7 @@ import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController(value = "/template")
 public class TemplateController {
