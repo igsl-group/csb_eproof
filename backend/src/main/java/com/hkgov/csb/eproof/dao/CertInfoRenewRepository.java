a73ece4c80d458c019028c6f6b933a72c26a96ed
David huang <David.Huang@igsl-group.com>
2024-08-01 14:24:53 +0800

    fix:certInfoRenew相关api开发

--- backend/src/main/java/com/hkgov/csb/eproof/dao/CertInfoRenewRepository.java
+++ backend/src/main/java/com/hkgov/csb/eproof/dao/CertInfoRenewRepository.java
@@ -19,49 +19,7 @@ public interface CertInfoRenewRepository extends JpaRepository<CertInfoRenew,Lon
     @Query("update CertInfoRenew set isDelete = true where id = :id")
     void updateIsDeleteById(@Param("id") Long id);
 
-    @Query(nativeQuery = true,value = """
-        SELECT * FROM cert_info_renew c WHERE
-        (
-            (c.cert_stage IN :certStageList) AND
-            (c.status IN :certStatusList)
-        )
-        AND
-        (
-            ( ?#{#searchDto.hkid} IS null OR c.hkid like %?#{#searchDto.hkid}% ) AND
-            ( ?#{#searchDto.passportNo} IS null OR c.passport_no like %?#{#searchDto.passportNo}% ) AND
-            ( ?#{#searchDto.canName} IS null OR c.name like %?#{#searchDto.canName}% ) AND
-            ( ?#{#searchDto.canCName} IS null OR c.cname like %?#{#searchDto.canCName}% ) AND
-            ( ?#{#searchDto.canEmail} IS null OR c.email like %?#{#searchDto.canEmail}% ) AND
-            ( ?#{#searchDto.examProfileSerialNo} IS null OR c.exam_profile_serial = ?#{#searchDto.examProfileSerialNo} ) AND
-            ( ?#{#searchDto.blnstGrade} IS null OR c.blnst_grade like %?#{#searchDto.blnstGrade}% ) AND
-            ( ?#{#searchDto.ueGrade} IS null OR c.ue_grade like %?#{#searchDto.ueGrade}% ) AND
-            ( ?#{#searchDto.ucGrade} IS null OR c.uc_grade like %?#{#searchDto.ucGrade}% ) AND
-            ( ?#{#searchDto.atGrade} IS null OR c.at_grade like %?#{#searchDto.atGrade}% ) AND
-            ( ?#{#searchDto.certValid} IS null OR c.is_valid = ?#{#searchDto.certValid} ) AND
-            ( ?#{#searchDto.onHold} IS null OR c.on_hold = ?#{#searchDto.onHold} )
-        )
-""",countQuery = """
- SELECT count(*) FROM cert_info_renew c WHERE
-        (
-            (c.cert_stage IN :certStageList) AND
-            (c.status IN :certStatusList)
-        )
-        AND
-        (
-            ( ?#{#searchDto.hkid} IS null OR c.hkid like %?#{#searchDto.hkid}% ) AND
-            ( ?#{#searchDto.passportNo} IS null OR c.passport_no like %?#{#searchDto.passportNo}% ) AND
-            ( ?#{#searchDto.canName} IS null OR c.name like %?#{#searchDto.canName}% ) AND
-            ( ?#{#searchDto.canCName} IS null OR c.cname like %?#{#searchDto.canCName}% ) AND
-            ( ?#{#searchDto.canEmail} IS null OR c.email like %?#{#searchDto.canEmail}% ) AND
-            ( ?#{#searchDto.examProfileSerialNo} IS null OR c.exam_profile_serial = ?#{#searchDto.examProfileSerialNo} ) AND
-            ( ?#{#searchDto.blnstGrade} IS null OR c.blnst_grade like %?#{#searchDto.blnstGrade}% ) AND
-            ( ?#{#searchDto.ueGrade} IS null OR c.ue_grade like %?#{#searchDto.ueGrade}% ) AND
-            ( ?#{#searchDto.ucGrade} IS null OR c.uc_grade like %?#{#searchDto.ucGrade}% ) AND
-            ( ?#{#searchDto.atGrade} IS null OR c.at_grade like %?#{#searchDto.atGrade}% ) AND
-            ( ?#{#searchDto.certValid} IS null OR c.is_valid = ?#{#searchDto.certValid} )AND
-            ( ?#{#searchDto.onHold} IS null OR c.on_hold = ?#{#searchDto.onHold} )
-        )
-""")
+    @Query("SELECT c FROM CertInfoRenew c ")
     Page<CertInfoRenew> certSearch(@Param("searchDto") CertSearchDto searchDto,
                               @Param("certStageList") @NotNull List<String> certStageList,
                               @Param("certStatusList") @NotNull List<String> certStatusList,
