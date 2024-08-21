ade0170c1933907d662a47a30f205ff2e99955e8
David huang <David.Huang@igsl-group.com>
2024-08-21 09:35:20 +0800

    fix:before删除exam_date

--- backend/src/main/java/com/hkgov/csb/eproof/dao/CombinedHistoricalResultBeforeRepository.java
+++ backend/src/main/java/com/hkgov/csb/eproof/dao/CombinedHistoricalResultBeforeRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CombinedHistoricalResultBeforeRepository extends JpaRepository<CombinedHistoricalResultBefore,Long> {
-    @Query("select c from CombinedHistoricalResultBefore c where c.hkid in :hkids or c.passport in :passports")
+    @Query("select c from CombinedHistoricalResultBefore c where c.hkid in :hkids or c.passport in :passports order by c.hkid,c.passport")
     List<CombinedHistoricalResultBefore> findByHkidIn(List<String> hkids, List<String> passports);
 }
