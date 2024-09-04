b740d5e65ee457b4cc07864766f7e72890ccf532
Wilfred Lai <wilfred.lai@igsl-group.com>
2024-09-04 16:17:16 +0800

    Use POI to generate PDFs

--- backend/src/main/java/com/hkgov/csb/eproof/service/impl/DocumentGenerateServiceImpl.java
+++ backend/src/main/java/com/hkgov/csb/eproof/service/impl/DocumentGenerateServiceImpl.java
@@ -72,8 +72,8 @@ public class DocumentGenerateServiceImpl implements DocumentGenerateService {
             case PDF ->{
                 logger.info("Start convert pdf.");
 //                returnBinary = docxUtil.convertDocxToPdf(tempDocxFile);
-//                returnBinary = docxUtil.convertDocxToPdf_POI(new ByteArrayInputStream(mergedDocxBinaryArray));
-                returnBinary = docxUtil.convertDocxToPdf2(mergedDocxBinaryArray);
+                returnBinary = docxUtil.convertDocxToPdf_POI(new ByteArrayInputStream(mergedDocxBinaryArray));
+//                returnBinary = docxUtil.convertDocxToPdf2(mergedDocxBinaryArray);
 //                returnBinary = docxUtil.convertDocxToPdf2(mergedDocxBinaryArray);
                 logger.info("Finish convert pdf.");
             }
