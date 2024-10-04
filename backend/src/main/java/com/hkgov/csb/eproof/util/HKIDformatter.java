60bdd97fdb801177f2a4b47b30eb2958131ddfc8
Wilfred Lai <wilfred.lai@igsl-group.com>
2024-10-05 04:24:50 +0800

    Update hkid formatter

--- backend/src/main/java/com/hkgov/csb/eproof/util/HKIDformatter.java
+++ backend/src/main/java/com/hkgov/csb/eproof/util/HKIDformatter.java
@@ -7,7 +7,7 @@ public class HKIDformatter {
 
   public String formatHkid(String hkid) {
     if (hkid != null && hkid.length() == 8) {
-      return hkid.substring(0, 7) + "(" + hkid.charAt(7) + ")";
+      return hkid.substring(0, 1) + "_" +hkid.substring(1, 7) + "(" + hkid.charAt(7) + ")";
     }
     return hkid;
   }
